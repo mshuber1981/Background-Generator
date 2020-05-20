b93c8f5d9696ad2ab848ce48884cd918c7ef9cbf
mshuber1981 <michael.huber1981@outlook.com>
2020-05-20 14:13:03 -0500

    Updates

---
+++ precache-manifest.c9791e395839f36ce40fdfc4e5ae4d3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59846631762d9aa67d70824b5aec840",
+    "url": "/Background-Generator/index.html"
+  },
+  {
+    "revision": "036c40b757987a7a987c",
+    "url": "/Background-Generator/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "389a538c16c936826a35",
+    "url": "/Background-Generator/static/css/main.b37ea3b4.chunk.css"
+  },
+  {
+    "revision": "036c40b757987a7a987c",
+    "url": "/Background-Generator/static/js/2.9a3beefa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Background-Generator/static/js/2.9a3beefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389a538c16c936826a35",
+    "url": "/Background-Generator/static/js/main.ceb628e5.chunk.js"
+  },
+  {
+    "revision": "bc933086911ed9e3d0cc",
+    "url": "/Background-Generator/static/js/runtime-main.7dec9529.js"
+  }
+]);
